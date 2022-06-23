6d5c6038f2025998f9aef455fa89ebea0ff27929
Shreyas Ghare <shreyas.ghare@globant.com>
2022-06-23 16:38:49 +0530

    copied angular JS code and updated the dependencies

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import * as angular from 'angular';
+import { setAngularJSGlobal } from '@angular/upgrade/static';
+
+import ajsApp from './app/app.module.ajs';
+
+setAngularJSGlobal(angular);
+
 if (environment.production) {
   enableProdMode();
 }
